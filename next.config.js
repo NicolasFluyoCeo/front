55f603098f99995e07de1d8dbba14eaf66d44211
Nicolás Eduardo Rondón Ortiz <nicolasrondon@nico.dev>
2024-10-06 22:00:44 -0500

    buid

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    distDir: 'out', // o 'public', según prefieras
+    reactStrictMode: true,
+    // Si tienes rutas dinámicas, elimina 'output: export'
+  }
+  
+  module.exports = nextConfig;
+  
--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    distDir: 'out', // or 'public' if you prefer
-    reactStrictMode: true,
-    // Remove 'output: export' if you have dynamic routes
-  }
-  
-  module.exports = nextConfig;
-  
