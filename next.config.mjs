cf563295ec8b1e62aea2bee8d4625555c159c87f
Nicolás Eduardo Rondón Ortiz <nicolasrondon@nico.dev>
2024-10-06 22:18:24 -0500

    build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    distDir: 'out' 
+    distDir: 'out', // O puedes dejar el predeterminado si no necesitas cambiar la salida
+    output: 'standalone', // Este es clave para que funcione SSR en Vercel
   }
   
   export default nextConfig;
