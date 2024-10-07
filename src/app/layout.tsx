d3d2de44c834e1b1498e2877f5fdaac31c0fa8fa
Nicolás Eduardo Rondón Ortiz <nicolasrondon@nico.dev>
2024-10-06 21:41:01 -0500

    update provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,15 +6,7 @@ import { UserProvider } from '@auth0/nextjs-auth0/client';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-    <UserProvider
-    auth0={
-      {
-        audience: 'https://api.fluyo.co',
-        domain: 'dev-zpu0bmc0ijrv1gne.us.auth0.com',
-        clientId: 'hm4ibUYwME78sGXvr6WS8pQ88BRqWiTb',
-        clientSecret: 'W7lkEGvpoAviYKAiLX4WyEjRl90iNaIf4VLngCKOD5_z2bK1U_JYSyyoygsjwj_x'
-      }
-    }>
+    <UserProvider>
       <body>{children}</body>
     </UserProvider>
     </html>
