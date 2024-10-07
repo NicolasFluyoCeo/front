d969091aa82c14d63806a5e8ab1708003907a181
Nicolás Eduardo Rondón Ortiz <nicolasrondon@nico.dev>
2024-10-06 22:46:19 -0500

    upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-export default  function Home() {
+import { getSession } from '@auth0/nextjs-auth0';
+import { redirect } from 'next/navigation';
+
+export default async function Home() {
   // Si estás usando componentes del lado del servidor
+  const session = await getSession();
+
+  // Si no hay usuario en la sesión, redirige
+  if (!session?.user) {
+    redirect('https://fluyo.co');
+    return;
+  }
 
   return (
     <main>
